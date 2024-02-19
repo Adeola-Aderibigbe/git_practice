3dc55369fc5bfa411dc91aeffea4e5369a4e6a72
Ebube <onyirimbaebubechi1234@gmail.com>
2024-02-19 15:52:24 +0100

    Integer types

---
+++ Project11/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+short firstNum = short.MinValue;
+short secondNum = short.MaxValue;
+Console.WriteLine(firstNum);
+Console.WriteLine(secondNum);
+
+ushort thirdNum = ushort.MaxValue;
+ushort fouthNum = ushort.MinValue;
+Console.WriteLine(thirdNum + " " + fouthNum);
+
+sbyte fifthNum = sbyte.MaxValue;
+sbyte sixthNum = sbyte.MinValue;
+Console.WriteLine(fifthNum + " " + sixthNum);
+
+byte seventhNum = byte.MaxValue;
+byte eightNum = byte.MinValue;
+Console.WriteLine(seventhNum + " " + eightNum);
+
+int ninthNum = int.MaxValue;
+int tenthNum = int.MinValue;
+Console.WriteLine(ninthNum + " " + tenthNum);
+
+uint eleventhNum = uint.MaxValue;
+uint twelvthNum = uint.MinValue;
+Console.WriteLine(eleventhNum + " " + twelvthNum);
+
+long thirteenthNum = long.MaxValue;
+long fouteenthNum = long.MinValue;
+Console.WriteLine(thirteenthNum + " " + fouteenthNum);
+
+ulong fifteenthNum = ulong.MaxValue;
+ulong sixteenthNum = ulong.MinValue;
+Console.WriteLine(fifteenthNum + " " + sixteenthNum);
+
+float seventeenthNum = float.MaxValue;
+float eighteenthNum = float.MinValue;
+Console.WriteLine(seventeenthNum + " " + eighteenthNum);
+
+double ninteenthNum = double.MaxValue;
+double twenteethNum = double.MinValue;
+Console.WriteLine(ninteenthNum + " " + twenteethNum);
+
+decimal twentyFirstNum = decimal.MaxValue;
+decimal twentySecondNum = decimal.MinValue;
+Console.WriteLine(twentyFirstNum + " " + twentySecondNum);

6a2d794c25ff68c5180c40b59667ea2ed1b3bc54
Ebube <onyirimbaebubechi1234@gmail.com>
2024-02-20 08:45:43 +0100

    Update discount sales

--- Testrun/Program.cs
+++ Testrun/Program.cs
@@ -1,20 +1,42 @@
 ﻿using System;
-using System.ComponentModel.Design;
-
-internal class program;
-// As a bookstore manager, you need to determine the discount for customers based on their total spending.
-// Discounts are applied as follows: no discount for spending less than $50, a 10% discount for spending between $50 and $100, a 15
-// %discount for spending between $100 and $200, and a 20% discount for spending over $200. The final payment is calculated by deducting
-// the discount amount from the total spending.
-int firstDiscount = 50;
-int secondDiscount = 100;
-int thirdDiscount = 200;
-int discount = ( );
-int totalSpending = (firstDiscount + secondDiscount + thirdDiscount);
-int finalPayment = (totalSpending - discount);
-
-Console.WriteLine(finalPayment);
+using System.ComponentModel.DataAnnotations;
 
 
+namespace Testrun
+{
+    class Program
+    {
+        static void Main()
+        {
+            //As a bookstore manager, you need to determine the discount for customers based on their total spending.
+            // Discounts are applied as follows: no discount for spending less than $50, a 10% discount for spending between $50 and $100, a 15
+            // %discount for spending between $100 and $200, and a 20% discount for spending over $200. The final payment is calculated by deducting
+            // the discount amount from the total spending.
+            Console.Write($"Enter total spending: $");
+            double totalSpending = Convert.ToDouble(Console.ReadLine());
+            double discount = 0;
+
+            if (totalSpending >= 50 && totalSpending < 100)
+            {
+                discount = 0.10;
+            }
+            else if (totalSpending >= 100 && totalSpending < 200)
+            {
+                discount = 0.15;
+            }
+            else if (totalSpending > 200)
+            {
+               discount = 0.20;
+            }
+
+            double discountAmount = totalSpending * discount;
+            double finalPayment = totalSpending - discount;
+            Console.WriteLine($"Discount {discount * 100}%");
+            Console.WriteLine($"Discount Amount {discountAmount}");
+            Console.WriteLine($"Final Payment {finalPayment}");
+
+        }
+    }
+}
 
 
